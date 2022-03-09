9b60535033658a68d6b6a4791bba1f2c662965f5
r0vina <rvnagale@gmail.com>
2022-03-09 13:46:15 +0100

    Roneet's first commit.

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ print("Hello World!")
 print("kasper test")
 
 print("Marcus was here!")
+
+print("An attempt was made.")
 #MADS
 #Nu virker det fandme YES YES YES !!!! KASPER GIVER ØL
 #virker det stadigvæk??

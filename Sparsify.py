ae5953476abfe2676b7f6314e631f20b4d943488
r0vina <rvnagale@gmail.com>
2022-03-16 14:38:26 +0100

    Cleaned and edited comments

--- Sparsify.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy
-
-
-dataset = pd.read_csv("")
--- small_data_ldm.py
+++ small_data_ldm.py
@@ -9,7 +9,7 @@ seller = df["Seller_address"].unique()
 buyer = df["Buyer_address"].unique()
 shape = (len(seller), len(buyer))
 
-# Create indices for users and movies
+# Create indices for Sellers and Buyers
 seller_cat = CategoricalDtype(categories=sorted(seller), ordered=True)
 buyer_cat = CategoricalDtype(categories=sorted(buyer), ordered=True)
 seller_index = df["Seller_address"].astype(seller_cat).cat.codes
@@ -17,10 +17,5 @@ buyer_index = df["Buyer_address"].astype(buyer_cat).cat.codes
 
 # Conversion via COO matrix
 coo = sparse.coo_matrix((df["Unique_id_collection"], (seller_index, buyer_index)), shape=shape)
-csr = to_csr
 
-#adj_matrix = pd.crosstab(small_dataset['Seller_address'], small_dataset['Buyer_address'])
-#idx = adj_matrix.columns.union(adj_matrix.index)
-#adj_matrix = adj_matrix.reindex(index = idx, columns=idx, fill_value=0)
-#print(adj_matrix)
 print(coo)

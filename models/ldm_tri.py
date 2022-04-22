2b2ecd52e840b7b0fdcf13925ea48eaa10de762c
khelverskovp <s203294@student.dtu.dk>
2022-04-22 19:55:40 +0200

    metrics til node classification

--- models/ldm_tri.py
+++ models/ldm_tri.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
 from sklearn import metrics
 from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, roc_auc_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -229,18 +230,32 @@ df['Category'] = np.loadtxt(dataset+'/sparse_v.txt')
 X_train, X_test, y_train, y_test = train_test_split(df, df['Category'], test_size=0.2, random_state=42)
 
 #Multinomial logistic regression
-logreg = lm.LogisticRegression(solver='lbfgs', multi_class='multinomial', tol=1e-4, random_state=1)
+logreg = lm.LogisticRegression(solver='lbfgs', multi_class='multinomial', random_state=1)
 logreg.fit(X_train, y_train)
 
-#Number of mis-classifications
+#Metrics
 print('Number of miss-classifications for Multinomial regression:\n\t {0} out of {1}'.format(np.sum(logreg.predict(X_test)!=y_test),len(y_test)))
+print('Accuracy for Multinomial regression:\n\t {0}'.format(logreg.score(X_test, y_test)))
+print('Confusion matrix for Multinomial regression:\n\t {0}'.format(confusion_matrix(y_test, logreg.predict(X_test))))
+print('ROC AUC for Multinomial regression:\n\t {0}'.format(roc_auc_score(y_test, logreg.predict(X_test))))
+print('Precision for Multinomial regression:\n\t {0}'.format(precision_score(y_test, logreg.predict(X_test))))
+print('Recall for Multinomial regression:\n\t {0}'.format(recall_score(y_test, logreg.predict(X_test))))
+
+
 
 #KNN
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train, y_train)
 
-#Number of mis-classifications
+#Metrics
 print('Number of miss-classifications for KNN:\n\t {0} out of {1}'.format(np.sum(knn.predict(X_test)!=y_test), len(y_test)))
+print('Accuracy for KNN:\n\t {0}'.format(knn.score(X_test, y_test)))
+print('Confusion matrix for KNN:\n\t {0}'.format(confusion_matrix(y_test, knn.predict(X_test))))
+print('ROC AUC for KNN:\n\t {0}'.format(roc_auc_score(y_test, knn.predict(X_test))))
+print('Precision for KNN:\n\t {0}'.format(precision_score(y_test, knn.predict(X_test))))
+print('Recall for KNN:\n\t {0}'.format(recall_score(y_test, knn.predict(X_test))))
+
+
 
 
 #################################################################
@@ -260,6 +275,7 @@ for i in u_labels:
     plt.scatter(df[label == i, 0], df[label == i, 1], s=10, c=np.random.rand(3,), label='cluster %d' % i)
 plt.legend()
 plt.show()
+
 
 
 

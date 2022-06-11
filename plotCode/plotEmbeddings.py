f41ad6387f3a5ad89fc7a37a5a54ff3ac380cf31
Marcus Roberto Nielsen <marcusrobertonielsen02@gmail.com>
2022-06-11 16:46:54 +0200

    made nicer scatter plots for 3d

--- plotCode/plotEmbeddings.py
+++ plotCode/plotEmbeddings.py
@@ -3,6 +3,11 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+
 colors = {'Games':'red', 'Art':'green', 'Collectible':'blue', 'Metaverse':'orange','Other':'purple','Utility':'brown'}
 
 class embedPlotter:
@@ -87,14 +92,18 @@ class embedPlotterTri(embedPlotter):
         plt.title("Scatter plot 2D - Tripartite model")
         plt.show()
 
-    def scatter3D(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(*zip(*self.l3),s=0.1,label="NFTs")
-        ax.scatter(*zip(*self.r3),s=0.1,label="Sellers")
-        ax.scatter(*zip(*self.u3),s=0.1,label="Buyers")
-        ax.legend(markerscale=15)
-        ax.set_title("Scatter plot 3D - Tripartite model")
+    def scatter3D(self,n_rot):
+        fig = plt.figure(figsize=(16,16),dpi=60)
+        for i in range(n_rot*n_rot):
+            ax= fig.add_subplot(n_rot,n_rot,i+1,projection='3d')
+            ax.scatter(*zip(*self.l3),s=0.1,label="NFTs")
+            ax.scatter(*zip(*self.r3),s=0.1,label="Sellers")
+            ax.scatter(*zip(*self.u3),s=0.1,label="Buyers")
+            ax.view_init(azim=i*360/(n_rot*n_rot))
+
+        lines, labels = fig.axes[-1].get_legend_handles_labels()
+        fig.suptitle("Scatter plot 3D - Tripartite model",fontsize=30,weight="bold")
+        fig.legend(lines, labels, loc = 'upper right',markerscale=30,borderpad=2,fontsize=20)
         plt.show()
     
     def categoryPlot(self):
@@ -120,14 +129,14 @@ path = "./results_final/ETH/2021-02"
 
 # embed plot structure bi
 epb = embedPlotterBi(path)
-epb.scatter()
-epb.scatter3D()
-epb.categoryPlot()
-epb.categoryPlot3D()
+#epb.scatter()
+#epb.scatter3D()
+#epb.categoryPlot()
+#epb.categoryPlot3D()
 
 # embed plot structure tri
 ept = embedPlotterTri(path)
-ept.scatter()
-ept.scatter3D()
-ept.categoryPlot()
-ept.categoryPlot3D()
+#ept.scatter()
+ept.scatter3D(10)
+#ept.categoryPlot()
+#ept.categoryPlot3D()

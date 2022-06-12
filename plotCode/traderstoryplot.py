70c4fd322b7a3abdc477fae40e8e338d3d83d56f
Mads <Mads@ringsted.com>
2022-06-12 10:51:00 +0200

    Fixed some comments

--- plotCode/traderstoryplot.py
+++ plotCode/traderstoryplot.py
@@ -35,24 +35,49 @@ sold_nfts = [int(i) for i in nft_id[np.where(seller_id == seller)[0]]]
 bought_nfts = [int(i) for i in nft_id[np.where(buyer_id == buyer)[0]]]
 print(len(sold_nfts),len(bought_nfts))
 
-# remove bought and sold NFTs to a common list
+# remove NFTs that have both been bought and sold to a common list
 common = list(set(sold_nfts) & set(bought_nfts))
 sold_nfts, bought_nfts = list(set(sold_nfts) - set(bought_nfts)), list(set(bought_nfts) - set(sold_nfts))
 
 s = 10
-# plot sold NFTs
-plt.scatter(*zip(*l2[sold_nfts]), marker="v", s =s, color = "green", label = "Sold NFTs")
-# plot bought NFTs
-plt.scatter(*zip(*l2[bought_nfts]), marker="*", s = s, color = "red", label = "Bought NFTs")
-# plot traded NFTs
-if len(common) > 0:
-    plt.scatter(*zip(*l2[common]), marker = "P", s = s, color = "black", label = "Traded NFTs")
-# plot the seller
-plt.scatter((*r2[seller]), marker = "o",s = 80, color = "blue", label = "Seller")
-# plot the buyer
-plt.scatter(*u2[buyer], marker="o",s=80, color = "purple", label = "Buyer")
-plt.legend()
-plt.show()
+def plot_2D_story():
+    l = torch.load(path + "/tri/results/D2/nft_embeddings").detach().numpy()
+    r = torch.load(path + "/tri/results/D2/seller_embeddings").detach().numpy()
+    u = torch.load(path + "/tri/results/D2/buyer_embeddings").detach().numpy()
+    # plot sold NFTs
+    plt.scatter(*zip(*l[sold_nfts]), marker="v", s =s, color = "green", label = "Sold NFTs")
+    # plot bought NFTs
+    plt.scatter(*zip(*l[bought_nfts]), marker="*", s = s, color = "red", label = "Bought NFTs")
+    # plot traded NFTs
+    if len(common) > 0:
+        plt.scatter(*zip(*l[common]), marker = "P", s = s, color = "black", label = "Traded NFTs")
+    # plot the seller
+    plt.scatter((*r[seller]), marker = "o",s = 80, color = "blue", label = "Seller")
+    # plot the buyer
+    plt.scatter(*u[buyer], marker="o",s=80, color = "purple", label = "Buyer")
+    plt.legend()
+    plt.show()
 
+def plot_3D_story():
+    l = torch.load(path + "/tri/results/D3/nft_embeddings").detach().numpy()
+    r = torch.load(path + "/tri/results/D3/seller_embeddings").detach().numpy()
+    u = torch.load(path + "/tri/results/D3/buyer_embeddings").detach().numpy()
 
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    # plot sold NFTs
+    ax.scatter(*zip(*l[sold_nfts]), marker="v", s =s, color = "green", label = "Sold NFTs")
+    # plot bought NFTs
+    ax.scatter(*zip(*l[bought_nfts]), marker="*", s = s, color = "red", label = "Bought NFTs")
+    # plot traded NFTs
+    if len(common) > 0:
+        ax.scatter(*zip(*l[common]), marker = "P", s = s, color = "black", label = "Traded NFTs")
+    # plot the seller
+    ax.scatter((*r[seller]), marker = "o",s = 80, color = "blue", label = "Seller")
+    # plot the buyer
+    ax.scatter(*u[buyer], marker="o",s=80, color = "purple", label = "Buyer")
+    ax.legend()
+    plt.show()
+plot_2D_story()
+plot_3D_story()
 

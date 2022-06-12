8773932f52e25800e662473ce1bb4b3706b47ae4
Marcus Roberto Nielsen <marcusrobertonielsen02@gmail.com>
2022-06-12 15:13:20 +0200

    small changes

--- plotCode/classification_and_clustering.py
+++ plotCode/classification_and_clustering.py
@@ -306,16 +306,23 @@ class EmbeddingPlotter(Formatter):
         self.u = torch.load("{path}/tri/results/D{dim}/buyer_embeddings".format(path=self.path,dim=self.dim)).detach().numpy()
 
     def make_scatter_plot(self,d1=1,d2=2,d3=3,plot3d=False):
-        fig = plt.figure(figsize=self.figsize)
+        self.fig = plt.figure(figsize=self.figsize)
         if plot3d:
             if self.dim < 3:
-                raise Exception("printhey")
-        
-        ax = fig.add_subplot(projection='3d')
-
+                raise Exception("Cannot plot {dim}D embeddings in 3D".format(dim=self.dim))
+            if not (d1 in range(1,self.dim+1) and d2 in range(1,self.dim+1) and d3 in range(1,self.dim+1)):
+                raise Exception("Invalid choice of coordinate dimensions")
+            self.ax = self.fig.add_subplot(projection='3d')
+        else:
+            if not (d1 in range(1,self.dim+1) and d2 in range(1,self.dim+1)):
+                raise Exception("Invalid choice of coordinate dimensions")
+            self.ax = self.fig.add_subplot()
+                
         if self.mtype == "bi":
+            self.make_scatter_plot_bi()
             if self.z is None or self.q is None:
                 self.load_embeddings_bi()
+            ax = 2
             
         elif self.mtype == "tri":
             if self.l is None or self.r is None or self.u is None:
@@ -405,4 +412,4 @@ class LinkPredictionPlotter(Formatter):
             plt.show()
 
 #test = LinkPredictionPlotter().ROC_PR_plot(show = True)
-test = LinkPredictionPlotter().ROC_PR_epoch(show = True)
+#test = LinkPredictionPlotter().ROC_PR_epoch(show = True)
